9d9fa8c92aa0e689c9e95ea9e1d446e2ad03af13
Zexion16 <alihma188@gmail.com>
2025-10-22 13:40:32 +0300

    Commit all tasks done

---
+++ src/test/java/monitoring/T01_RandomUserApi.java
@@ -0,0 +1,43 @@
+package monitoring;
+
+import base_urls.RandomUserApi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pojos.randomUserPojo.RandomUserResponse;
+import pojos.randomUserPojo.ResultsItem;
+import utilities.ObjectMapperUtils;
+
+import static io.restassured.RestAssured.given;
+
+public class T01_RandomUserApi extends RandomUserApi {
+
+    /*
+    Task 1: Random User API - GET Request with POJO Deserialization
+    Objective: Write an automation test that validates user data from a random user API endpoint.
+    Requirements:
+    Send a GET request to https://randomuser.me/api
+    The response will contain random user information in nested JSON structure
+    Deserialize the response into a POJO class
+    Assert that the following fields are NOT null:
+    Email
+    Username
+    Password
+    Medium picture URL
+     */
+
+    @Test
+    public void randomUserApiTest(){
+
+        Response response = given(spec).get();
+//        response.prettyPrint();
+
+        RandomUserResponse actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), RandomUserResponse.class);
+        Assert.assertNotNull(actualData.getResults().getFirst().getEmail());
+        Assert.assertNotNull(actualData.getResults().getFirst().getLogin().getUsername());
+        Assert.assertNotNull(actualData.getResults().getFirst().getLogin().getPassword());
+        Assert.assertNotNull(actualData.getResults().getFirst().getPicture());
+    }
+}
---
+++ src/test/java/monitoring/T02_PetStorePojoTest.java
@@ -0,0 +1,27 @@
+package monitoring;
+
+import org.testng.annotations.Test;
+
+public class T02_PetStorePojoTest {
+    /*
+    Task 2: Petstore API - Create a Pet with POJO Payload
+Objective: Create a new pet in the Petstore API using a POJO as the request payload.
+Requirements:
+Reference the API documentation at https://petstore.swagger.io/
+Create a POJO class representing a Pet object with properties like:
+id
+name
+category
+photoUrls
+status
+tags
+Send a POST request to the create pet endpoint with your POJO as the body
+Assert that the response status code is successful (200 or 201)
+Assert that the returned pet object contains the data you sent
+     */
+
+    @Test
+    public void petStorePojoTest() {
+
+    }
+}
